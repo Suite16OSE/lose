dca4c71c1503bebc06584ea1fd2cc2944399a06c
Kirn Gill II <segin2005@gmail.com>
2024-05-20 17:12:30 -0500

    Beginning of command line parsing, reformatted comments

--- lose.asm
+++ lose.asm
@@ -8,45 +8,76 @@ section .data
     s_newline    db 0x0D, 0x0A, '$'
 
 section .bss
-    a_psp:       resw 2
+    np_psp:       resw 2
+    i_mode:       resb 1
+
 
 section .text
 start:
-    mov ax, 0
+    xor ax, ax
     call print_cmdline
 
     ; start parsing the command line 
-    mov al, [0x80]      ; strlen(GetCommandLine())
+    mov ah, [0x80]          ; strlen(GetCommandLine())
 parse_flags:
+    xor cx, cx              ; zero our counter
+    xor bh, bh
+    mov bl, 0x81            ; load command line string base address
+    lea si, [bx]
+.loop:    
+    lodsb                   ; read a byte
+    cmp al, '/'             ; is it a flag? 
+    jne .afterflag          ; if not, skip.
+.flag:
+    lodsb                   ; load the flag
+
+
+    mov byte [si-2], 0x20   ; replace flag with spaces for KERNEL 
+    mov byte [si-1], 0x20  
+.afterflag:
+    inc cl                  
+    cmp ah, cl
+    jne .loop
+.end:
+    mov al, 1
+    call print_cmdline      ; print modified command line passed to KERNEL 
+    call exit               ; exit LOSE.COM
 
 print_cmdline:
-    mov dx, s_old       ; load "Old" string
-    cmp ax, 0           ; new or old command line? old is zero.
-    je print_rest       ; print it and go
-print_new:
-    mov dx, s_new       ; load "New" string instead
-print_rest:
-    mov ah, 9           ; write string to stdout
-    int 0x21            ; call DOS
-    mov dx, s_cmdline   ; rest of string
-    int 0x21            ; call DOS
-print_cmdchars:
-    xor bh, bh          ; zero b-high
-    mov bl, [0x80]      ; read length of command line from dos. max of 127
-    add bl, 0x81        ; add 0x81 (start of cmdline in PSP) to length to get end marker
-    mov byte [bx], '$'  ; replace with $ for DOS print call
-    push bx             ; save address so DOS doesn't mangle it
-    mov dx, 0x81        ; set address of string to print to PSP command line
-    int 0x21            ; ah=0x09, print string to stdout
-    pop bx              ; restore address to terminator character
-    mov byte [bx], 0x0d ; restore original 0x0d terminator
-print_newline:
-    mov dx, s_newline   ; newline
-    int 0x21            ; call DOS
+    push bx                 ; save registers
+    push dx 
+    mov dx, s_old           ; load "Old" string
+    cmp ax, 0               ; new or old command line? old is zero.
+    je .print_rest          ; print it and go
+.print_new:
+    mov dx, s_new           ; load "New" string instead
+.print_rest:
+    mov ah, 9               ; write string to stdout
+    int 0x21                ; call DOS
+    mov dx, s_cmdline       ; rest of string
+    int 0x21                ; call DOS
+.print_cmdchars:
+    xor bh, bh              ; zero b-high
+    mov bl, [0x80]          ; read length of command line from dos. max of 127
+    add bl, 0x81            ; add 0x81 (start of cmdline in PSP) to length to get end marker
+    mov byte [bx], '$'      ; replace with $ for DOS print call
+    push bx                 ; save address so DOS doesn't mangle it
+    mov dx, 0x81            ; set address of string to print to PSP command line
+    int 0x21                ; ah=0x09, print string to stdout
+    pop bx                  ; restore address to terminator character
+    mov byte [bx], 0x0d     ; restore original 0x0d terminator
+.print_newline:
+    mov dx, s_newline       ; newline
+    int 0x21                ; call DOS
+    pop dx                  ; restore registers
+    pop bx
+    ret
+
+show_help:
     ret
 
 exit:
     ; Terminate program, value in al
     mov ah, 0x4C  ; DOS function 4Ch - terminate program
     int 0x21      ; Call DOS interrupt
-    nop           ; end of .text canary
+    nop           ; end of .text canary - data follows!
