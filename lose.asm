a3059fea85fc9bdfc1936fef8e341a8af4faa5fd
Kirn Gill II <segin2005@gmail.com>
2024-05-27 17:54:24 -0500

    more reformatting

--- lose.asm
+++ lose.asm
@@ -28,6 +28,8 @@ section .data
     s_dosx:         db 'SYSTEM\\DOSX.EXE', 0
     s_wswap:        db 'SYSTEM\\WSWAP.EXE', 0
     s_win386:       db 'SYSTEM\\WIN386.EXE', 0
+    s_errmem:       db 'Error reallocating memory!$'
+
 section .bss
     np_psp:         resw 2
     i_mode:         resb 1
