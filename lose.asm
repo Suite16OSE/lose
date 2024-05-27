13aa76dd7d31ef58562f2afdede03c3aea589929
Kirn Gill II <segin2005@gmail.com>
2024-05-27 17:54:09 -0500

    reformatting strings

--- lose.asm
+++ lose.asm
@@ -25,10 +25,9 @@ section .data
     s_help6:        db '   /?        Displays this help.$'
     s_help7:        db 'Submit bug reports or patches at https://github.com/Suite16OSE/lose$'
     s_helps:        db 'Starts Suite16 in $'
-    s_dosx:         db 'SYSTEM\\DOSX.EXE$'
-    s_wswap:        db 'SYSTEM\\WSWAP.EXE$'
-    s_win386:       db 'SYSTEM\\WIN386.EXE$'
-    s_errmem:       db 'Error reallocating memory!$'
+    s_dosx:         db 'SYSTEM\\DOSX.EXE', 0
+    s_wswap:        db 'SYSTEM\\WSWAP.EXE', 0
+    s_win386:       db 'SYSTEM\\WIN386.EXE', 0
 section .bss
     np_psp:         resw 2
     i_mode:         resb 1
